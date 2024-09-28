6848b8886ce8d3c20e32ac7ba54ee6bf58961b68
Crypto-Xime <ximenanuyens@hotmail.com>
2024-09-28 04:49:31 +0300

    Add files via upload

---
+++ Strings_and_string_indexing.py
@@ -0,0 +1,14 @@
+#String indexing
+
+name = 'Guatemala Spring'
+print(name[0])
+print(name[8])
+print(name[10])
+print(name[-1])
+print(name[11:16])
+print(name[4:9])
+print(name[::-1])
+print(name[0] + name[2] + name [4] + name [6] + name [8] + ' ' + name[11] + name[13] + name[15])
+
+
+
