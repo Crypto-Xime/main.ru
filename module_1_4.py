f4a433fb18efb87d4cfb1786b0d13e8bdc15d885
Crypto-Xime <ximenanuyens@hotmail.com>
2024-09-29 03:24:49 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,27 @@
+# Greeting using concatenation string
+my_string1 = "Hello,"
+my_string2 = "Teacher"
+print(my_string1 + my_string2)
+
+# Changing Case String Method for Upper Case
+result = (my_string1 + my_string2).upper()
+print(result)
+
+# Changing Case String Method for Lower Case
+result = (my_string1 + my_string2).lower()
+print(result)
+
+# String configuration
+name = input("Enter your name:")
+current_year = input("What year is it?")
+name_of_the_university = input("What is the name of your university?")
+print("Welcome," , name)
+
+# Print The First and Last Characters of the Concatenated String
+my_string1 = "Hello,"
+my_string2 = "Teacher"
+my_string = my_string1 + my_string2
+first_char = my_string1[0]   #First Character
+last_char = my_string2[-1]   #Last Character
+print("First Character:" , first_char)
+print("Last Character:" , last_char)
