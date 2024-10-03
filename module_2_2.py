41aac319305014e156d223d667dd35a1a28e9525
Crypto-Xime <ximenanuyens@hotmail.com>
2024-10-04 02:50:59 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,43 @@
+name = input("What is the university name:")
+if name == "Urban":
+    print('Hello, Teacher')
+else: print('Hello Teacher at', name)
+
+                      # Read input values Example 1
+first = int(123)
+second = int(456)
+third = int(789)
+
+# Check conditions
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+                      # Read input values Example 2
+first = int(30)
+second = int(19)
+third = int(30)
+
+# Check conditions
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+                    # Read input values Example 3
+first = int(300)
+second = int(300)
+third = int(300)
+
+# Check conditions
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
