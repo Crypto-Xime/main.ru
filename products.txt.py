d0801cf8d27f65450dd99f84e835916ad23f4f0d
Crypto-Xime <ximenanuyens@hotmail.com>
2024-12-06 23:07:40 +0300

    Add files via upload

---
+++ products.txt.py
@@ -0,0 +1,7 @@
+from PhytonProyect7.module_7_1 import Product
+
+p1 = Product('Tomato', 5.50, 'Vegetables', 'Alice')
+p2 = Product('Spaghetti', 4.40, 'Carbohydrates', 'Bob')
+p3 = Product('Potato', 6.50, 'Vegetables', 'Charlie')
+p4 = Product('Meat', 7.80, 'Protein', 'Dave')
+p5 = Product('Strawberry', 9.53, 'Fruits', 'Eve')
